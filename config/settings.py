c4ef89b840f12dbd87b7e7e6013885344054cbdf
Armughan Khan <khanarmughan71@gmail.com>
2025-10-01 16:50:28 +0500

    Updated resume button and small fixes

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
